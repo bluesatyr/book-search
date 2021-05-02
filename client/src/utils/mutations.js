2b13507a674842d9c51f803832fd1b0558e297ba
bluesatyr <shawnevans.music@gmail.com>
2021-05-02 12:14:29 -0400

    add queries and mutations to client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,53 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String! ) {
+    addUser(username: $username, email: $email, password: $password){
+        _id
+      username
+      email
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($title: String!, $bookId: String!, $authors: [String], $description: String!, $link: String, $image: String){
+    saveBook(bookData: {title: $title, bookId: $bookId, authors: $authors, description: $description, link: $link, image: $image}) {
+      _id
+      username
+      savedBooks {
+        _id
+        title
+        authors
+        bookId
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId){
+      _id
+      username
+      savedBooks {
+        _id
+        title
+        authors
+        bookId
+      }
+    }
+  }
+`;
